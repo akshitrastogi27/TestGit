f9f1e13d7a87aa34afce3c450d5fbee6957c6ace
akshit <akshit.rastogi27@gmail.com>
2020-11-18 10:29:32 +0530

    First Commit

---
+++ MavanJava/src/test/java/AkshitTest/MavanJava/AppiumTest.java
@@ -0,0 +1,17 @@
+package AkshitTest.MavanJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAnderoid() {
+		System.out.println("NativeAppAndriod");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+
+}
---
+++ MavanJava/src/test/java/AkshitTest/MavanJava/RestAPITest.java
@@ -0,0 +1,16 @@
+package AkshitTest.MavanJava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void postJira() {
+		System.out.println("PostJira");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
